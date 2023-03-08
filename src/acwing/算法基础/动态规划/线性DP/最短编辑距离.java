b551164b376d80cdb4d88d50780fba953c92fda9
风亦未止 <769522974@qq.com>
2023-03-08 20:19:23 +0800

    动态规划

--- src/acwing/算法基础/动态规划/线性DP/最短编辑距离.java
+++ src/acwing/算法基础/动态规划/线性DP/最短编辑距离.java
@@ -1,5 +1,7 @@
 package acwing.算法基础.动态规划.线性DP;
 
+import java.util.Scanner;
+
 /**
  * https://www.acwing.com/problem/content/904/
  *给定两个字符串 A 和 B，现在要将 A 经过若干操作变为 B，可进行的操作有：
@@ -38,9 +40,30 @@ package acwing.算法基础.动态规划.线性DP;
  * @date 2023/3/7 20:07
  */
 public class 最短编辑距离 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner scanner =  new Scanner(System.in);
+        int m = scanner.nextInt();
+        String word1 = scanner.next();
+        int n = scanner.nextInt();
+        String word2 = scanner.next();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
 
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
 
-
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]))+1;
+                }
+            }
+        }
+        System.out.print(dp[m][n]);
     }
 }
